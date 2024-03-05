c0e7d5edf374af3ae605c689ea5c2f39e6b94f7b
setab <saidulislamsetab@gmail.com>
2024-03-06 00:18:53 +0600

    added main.js api for cat

--- mod.js
+++ mod.js
@@ -1,8 +1,1 @@
-// import "./style.css";
-// import { getRandomCat } from "module";
-// const require = getRandomCat(import.meta.url);
-// global.require = require;
-// // console.log(getRandomCat);
-// // const getRandomCat = require("random-cat-img");
-// // getRandomCat().then((data) => console.log(data.message));
-// // console.log("wow");
+
